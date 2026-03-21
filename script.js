4d5b90d92bb239a18c2ffc75afa4b2fdc41429d9
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-21 09:57:17 +0400

    Update script.js

--- script.js
+++ script.js
@@ -271,3 +271,12 @@ window.addEventListener("load", ()=>{
   }
 
 });
+
+
+function goBack(){
+  if(document.referrer !== ""){
+    history.back();
+  }else{
+    window.location.href = "index.html";
+  }
+}
